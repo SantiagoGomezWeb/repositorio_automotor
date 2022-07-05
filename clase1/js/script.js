82a5a86c8a86f2dbf9bf741eed98b9e2a91e4775
Santiago Gomez <santiago.daniel.gomez@gmail.com>
2022-07-05 15:19:49 -0300

    primer commit

---
+++ clase1/js/script.js
@@ -0,0 +1,152 @@
+// Sintaxis básica de Javascript = css = CTRL K C
+
+/**   SHIFT ALT A
+ asdasdas
+ asdasd
+ asd
+ asdasd
+*/
+
+/* let numeroA = 4;
+let numeroB = 9;
+
+let suma = numeroA + numeroB;
+console.log(suma);
+
+let nombreUsuario = prompt('Ingresá tu nombre');
+let edadUsuario = prompt('Ingresá tu edad');
+
+console.log(nombreUsuario);
+alert('Bienvenido, ' + nombreUsuario)
+
+let edadActual = parseInt(prompt('Ingrese edad'));
+let edadEn5 = edadActual + 5;
+alert('En 5 años tendràs:'+edadEn5+' años')
+ */
+
+// clase 2 //
+
+
+
+
+// BOOKLET
+// JAVASCRIPT 1
+/* // Act #1
+let nombre = 'Homero';
+let apellido = 'Simpson';
+let edad = 50;
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+
+// Act #2
+const ciudad1 = 'Cordoba';
+const ciudad2 = 'Calera';
+
+console.log(ciudad1);
+console.log(ciudad2);
+
+// Act #3
+let nombreCarnet = 'Bart Simpson';
+let direccionCarnet = '742 Evergreen Terr.';
+let paisCarnet = 'Springfield, USA';
+const codigoCarnet = 'B47U89RE243';
+
+let carnet = 'Codigo: ' + codigoCarnet + ' ' +
+             'Nombre: ' + nombreCarnet + ' ' +
+             'Dirección: ' + direccionCarnet + ' ' +
+             'Pais: ' + paisCarnet;
+
+console.log(carnet);
+
+// Act #4
+let integranteNro1 = prompt('Ingresá tu nombre');
+let integranteNro2 = prompt('Ingresá tu nombre');
+let integranteNro3 = prompt('Ingresá tu nombre');
+
+let familia = 'Integrante Nro 1: ' + integranteNro1 + ' ' + 
+              'Integrante Nro 2: ' + integranteNro2 + ' ' + 
+              'Integrante Nro 3: ' + integranteNro3;
+
+alert('Los integrantes de la Familia son: ' + familia);
+
+
+// Act #5
+let precio = parseFloat(prompt('Ingrese Precio: '));
+
+let precio1conDes = precio * ((100 - 20) / 100);
+let precio2conDes = precio * ((100 - 30) / 100);
+
+let mensaje = 'Precio: $' + precio + ' con 20% de Descuento: $' + precio1conDes + ' ' +
+              'Precio: $' + precio + ' con 30% de Descuento: $' + precio2conDes;
+alert(mensaje);
+ 
+
+// JAVASCRIPT 2
+// Act #1
+let entrada = prompt('Ingrese un Nombre: ');
+let nombre = 'Santiago';
+
+if (entrada == nombre) {
+    alert('FUI YO');
+} else {
+    alert('NO FUI YO');
+}
+
+// Act #2
+let entrada = prompt('Ingrese la letra: ');
+if ((entrada == 'Y') || (entrada == 'y')) {
+    alert('CORRECTO');
+} else {
+    alert('ERROR');
+}
+
+
+// Act #3
+let entrada = prompt('Ingrese un numero: ');
+
+if (entrada == 1) {
+    alert('Elegiste a Homero');
+}else if (entrada == 2) {
+    alert('Elegiste a Marge');
+}else if (entrada == 3) {
+    alert('Elegiste a Bart');
+}else if (entrada == 4) {
+    alert('Elegiste a Lisa');
+}
+else {
+    alert('Error');
+}
+
+
+let numero = parseFloat(prompt('Ingrese un Nro:'));
+
+if ((numero >= 0) && (numero <= 1000)) {
+    alert('Presupuesto Bajo');
+} else if ((numero >= 1001) && (numero <= 3000)){
+    alert('Presupuesto medio');
+} else if (numero > 3000){
+    alert('Presupuesto Alto');
+} else {
+    alert('Error');
+}
+
+
+let producto1 = prompt('Ingrese producto 1: ');
+let producto2 = prompt('Ingrese producto 2: ');
+let producto3 = prompt('Ingrese producto 3: ');
+
+let lista = 'Producto 1: ' + producto1 + ' ' +
+            'Producto 2: ' + producto2 + ' ' +
+            'Producto 3: ' + producto3;
+
+if ((producto1 != '') && (producto2 != '') && (producto3 != '')) {
+    alert(lista);
+} else {
+    alert('Error: es necesario cargar todos los productos');
+}
+*/
+
+// JAVASCRIPT 3
+// Act #1
